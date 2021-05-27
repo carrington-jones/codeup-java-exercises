33f55acb60a14fef81a74a0db39307dec70fcb30
Carrington Jones <carrington.jones47@gmail.com>
2021-05-27 09:30:43 -0500

    Completed Java string exercises

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -10,5 +10,10 @@ public class StringExercise {
         String check = "Check \"this\" out!, \"s inside of \"s";
         System.out.println(check);
 
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String backslash = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(backslash);
     }
 }
