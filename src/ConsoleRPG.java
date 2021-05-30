fabede4468ac67358b67aa1f64895bb05ed6dac0
Carrington Jones <carrington.jones47@gmail.com>
2021-05-30 16:40:18 -0500

    Continued to work on console RPG

--- src/ConsoleRPG.java
+++ src/ConsoleRPG.java
@@ -1,36 +1,58 @@
 import java.util.Scanner;
 
 public class ConsoleRPG {
-    public static void begin (){ //method starts game
+    public static void begin() { //method starts game
         Scanner scanner = new Scanner(System.in);
         System.out.println("Are you ready to begin your adventure? Yes/No");
         String userInputBegin = scanner.nextLine();
-        if (userInputBegin.equalsIgnoreCase("yes")){
+        if (userInputBegin.equalsIgnoreCase("yes")) {
             getName();
-            System.out.println("Your adventure has begun and you have made your start towards the castle");
-        } if (userInputBegin.equalsIgnoreCase("no")){
+            System.out.println("Your adventure has begun and you have made your start towards the castle. You have 3 potions in your inventory!");
+            leftOrRight1();
+        }
+        if (userInputBegin.equalsIgnoreCase("no")) {
             System.out.println("Okay goodbye!");
         }
-    }public static void getName(){ //method gets users name
+    }
+
+    public static void getName() { //method gets users name
         Scanner scanner = new Scanner(System.in);
         System.out.println("Great! What is your name?");
         String userName = scanner.nextLine();
         System.out.printf("Hello %s!%n", userName);
     }
 
-    public static void leftOrRight1(){ //1st user choice
+    public static void leftOrRight1() { //1st user choice
         System.out.println("There is a fork in the road. Would you like to go left or right?");
         Scanner scanner = new Scanner(System.in);
         String userLeftOrRight1 = scanner.nextLine();
-        if (userLeftOrRight1.equalsIgnoreCase("left")){
-
-        } if (userLeftOrRight1.equalsIgnoreCase("right")){
-
+        if (userLeftOrRight1.equalsIgnoreCase("left")) {
+            System.out.println("You have encountered an orc! Attack or run away?");
+            String userOrcAttack = scanner.nextLine();
+            //code for orc fight
+        }
+        if (userLeftOrRight1.equalsIgnoreCase("right")) {
+            System.out.println("You have encountered a rogue! Attack or run away?");
+            String userOrcAttack = scanner.nextLine();
+            //code for rogue fight
         }
 
     }
+
+    public static int potionCountStart = 3;
+
+    public static void drinkPotion() {
+        if (potionCountStart == 0) {
+            System.out.println("You do not have any potions left!");
+        } else {
+            potionCountStart--;
+            System.out.println("You drank a potion! Your health is now " + (heroStartingHealth + 25) + " You have " + potionCountStart + " remaining");
+        }
+
+    }
+
     //Starting points for hero fights. Based on right and left choices of user
-public static int heroStartingHealth = 100;
+    public static int heroStartingHealth = 100;
     public static int orcStartingHealth = 25;
     public static int rogueStartingHealth = 25;
     public static int trollStartingHealth = 50;
@@ -38,9 +60,10 @@ public static int heroStartingHealth = 100;
     public static int dragonStartingHealth = 75;
     public static int knightKingStartingHealth = 75;
 
-//main methods
+    //main methods
     public static void main(String[] args) {
-    begin();
+        begin();
         System.out.println(heroStartingHealth);
+        drinkPotion();
     }
 }
