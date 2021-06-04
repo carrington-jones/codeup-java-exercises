9af66e1e5ca06d6c014cffd568d488b2c25ffe3f
Carrington Jones <carrington.jones47@gmail.com>
2021-06-04 14:12:53 -0500

    Updated inheritance and polymorphism exercise after walkthrough

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -5,11 +5,12 @@ public class Square extends Rectangle{
     public Square (int side){
         super(side, side);
     }
-
+    @Override
     public int getArea(){
     return (int) (Math.pow(this.length, 2)); //this.length is inherited from Rectangle class because of extends
     }
 
+    @Override
     public int getPerimeter(){
     return 4 * this.length;
     }
